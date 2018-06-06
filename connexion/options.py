a4364d964e504e7617667550c161a7afec88391c
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-06-05 20:52:24 -0700

    refactor spec version from options.py

--- connexion/options.py
+++ connexion/options.py
@@ -5,8 +5,16 @@ MODULE_PATH = pathlib.Path(__file__).absolute().parent
 
 
 class ConnexionOptions(object):
-    def __init__(self, options=None):
+
+    def __init__(self, options=None, oas_version=(2,)):
         self._options = {}
+        if oas_version >= (3, 0, 0):
+            self.openapi_spec_name = '/openapi.json'
+            self.swagger_ui_local_path = MODULE_PATH / 'vendor' / 'swagger-ui-3'
+        else:
+            self.openapi_spec_name = '/swagger.json'
+            self.swagger_ui_local_path = MODULE_PATH / 'vendor' / 'swagger-ui-2'
+
         if options:
             self._options.update(filter_values(options))
 
@@ -38,24 +46,6 @@ class ConnexionOptions(object):
         return self._options.get('swagger_json', True)
 
     @property
-    def openapi_spec_version(self):
-        # type: () -> str
-        """
-        The version of the OpenAPI Specification
-
-        Default: "2.0.0"
-        """
-        return self._options.get('openapi_spec_version', "2.0.0")
-
-    @property
-    def openapi_spec_major_version(self):
-        # type: () -> str
-        """
-        The major version of the OpenAPI Specification (likely either "2" or "3")
-        """
-        return self._options.get('openapi_spec_version', "2.0.0").split(".")[0]
-
-    @property
     def openapi_console_ui_available(self):
         # type: () -> bool
         """
@@ -76,11 +66,7 @@ class ConnexionOptions(object):
 
         Default: /openapi.json for openapi3, otherwise /swagger.json
         """
-        major_version = self.openapi_spec_major_version
-        default_path = "/swagger.json"
-        if major_version == "3":
-            default_path = "/openapi.json"
-        return self._options.get('openapi_spec_path', default_path)
+        return self._options.get('openapi_spec_path', self.openapi_spec_name)
 
     @property
     def openapi_console_ui_path(self):
@@ -101,10 +87,7 @@ class ConnexionOptions(object):
 
         Default: Connexion's vendored version of the OpenAPI Console UI.
         """
-        major_version = self.openapi_spec_major_version
-        ui_path = MODULE_PATH / 'vendor' / 'swagger-ui-{version}'.format(
-            version=major_version)
-        return self._options.get('swagger_path', ui_path)
+        return self._options.get('swagger_path', self.swagger_ui_local_path)
 
 
 def filter_values(dictionary):
