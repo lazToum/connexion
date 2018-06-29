1c4b9ad8ba604a99951535c0aa87bd78ad9624bf
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-06-29 04:00:19 -0700

    use abc

--- connexion/decorators/uri_parsing.py
+++ connexion/decorators/uri_parsing.py
@@ -1,6 +1,8 @@
 # Decorators to split query string
+import abc
 import functools
 import logging
+import six
 
 from .decorator import BaseDecorator
 
@@ -14,24 +16,26 @@ QUERY_STRING_DELIMITERS = {
 }
 
 
+@six.add_metaclass(abc.ABCMeta)
 class BaseParser(BaseDecorator):
     def __init__(self, param_defns):
         """
         """
         self.param_defns = param_defns
 
-    @property
+    @abc.abstractproperty
     def param_schemas(self):
-        return {k: v.get("schema", {}) for k, v in self.param_defns.items()}
+        """
+        """
 
     def __repr__(self):
         """
         :rtype: str
         """
-        return '<BaseParser>'
+        return "<{classname}>".format(classname=self.__class__.__name__)
 
-    @staticmethod
-    def _resolve_param_duplicates(values, param_defn):
+    @abc.abstractmethod
+    def _resolve_param_duplicates(self, values, param_defn):
         """ Resolve cases where query parameters are provided multiple times.
             The default behavior is to use the last-defined value.
             For example, if the query string is '?a=1,2,3&a=4,5,6' the value of
@@ -40,27 +44,11 @@ class BaseParser(BaseDecorator):
             (swagger2) then the duplicate values are concatenated together and
             `a` would be "1,2,3,4,5,6".
         """
-        try:
-            style = param_defn['style']
-            delimiter = QUERY_STRING_DELIMITERS.get(style, ',')
-            is_form = (style == 'form')
-            explode = param_defn.get('explode', is_form)
-            if explode:
-                return delimiter.join(values)
-        except KeyError:
-            if param_defn.get('collectionFormat') == 'multi':
-                return ','.join(values)
-        # default to last defined value
-        return values[-1]
 
-    @staticmethod
-    def param_split(value, param_defn):
-        try:
-            style = param_defn['style']
-            delimiter = QUERY_STRING_DELIMITERS.get(style, ',')
-            return value.split(delimiter)
-        except KeyError:
-            return value.split(',')
+    @abc.abstractmethod
+    def param_split(self, value, param_defn):
+        """
+        """
 
     def resolve_params(self, params, resolve_duplicates=False): 
         resolved_param = {}
@@ -117,7 +105,46 @@ class BaseParser(BaseDecorator):
         return wrapper
 
 
-class Swagger2QueryParser(BaseParser):
+class OpenAPIURIParser(BaseParser):
+
+    @property
+    def param_schemas(self):
+        return {k: v.get("schema", {}) for k, v in self.param_defns.items()}
+
+    @staticmethod
+    def param_split(value, param_defn):
+        try:
+            style = param_defn['style']
+            delimiter = QUERY_STRING_DELIMITERS.get(style, ',')
+            return value.split(delimiter)
+        except KeyError:
+            return value.split(',')
+
+    @staticmethod
+    def _resolve_param_duplicates(values, param_defn):
+        """ Resolve cases where query parameters are provided multiple times.
+            The default behavior is to use the last-defined value.
+            For example, if the query string is '?a=1,2,3&a=4,5,6' the value of
+            `a` would be "4,5,6".
+            However, if 'explode' is true, or the 'collectionFormat' is 'multi'
+            (swagger2) then the duplicate values are concatenated together and
+            `a` would be "1,2,3,4,5,6".
+        """
+        try:
+            style = param_defn['style']
+            delimiter = QUERY_STRING_DELIMITERS.get(style, ',')
+            is_form = (style == 'form')
+            explode = param_defn.get('explode', is_form)
+            if explode:
+                return delimiter.join(values)
+        except KeyError:
+            if param_defn.get('collectionFormat') == 'multi':
+                return ','.join(values)
+        # default to last defined value
+        return values[-1]
+
+
+class Swagger2URIParser(BaseParser):
 
     @property
     def param_schemas(self):
